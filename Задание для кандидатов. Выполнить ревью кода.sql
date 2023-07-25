c3fc5419cfe2072357429aa5ae9607199252bc4c
Rust <105740514+Chelmen@users.noreply.github.com>
2023-07-25 16:28:39 +0300

    Update Задание для кандидатов. Выполнить ревью кода.sql
    1) строка 7 - все переменные следуют задавать в одном объявлении
    2) строка 9 - отступ на комментарии не соответствует отступу кода, к которому относиться
    3) строка 10 - if и else с begin и end должны быть на одном уровне
    4) строка 12 - переменную syn.ImportFile стоит назвать imf
    5) строка 17 - отступов перед set нет
    6) строка 20 - пустая строка перед return
    7) строка 24 - между скобкой и созданием объекта нужен пробел
    8) строка 24 - create table стоит написать строчными буквами
    9) строка 26 - после знака "--" должен быть пробел
    10) строка 36 - пропущено присваивание "as" перед "cs"
    11) строка 47-50 - для многострочных комментариев используется конструкция /* */
    12) строка 66 - для повышения читаемости кода следует перенести часть условий на новую строку

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -4,25 +4,26 @@ as
 set nocount on
 begin
 	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
-	declare @ErrorMessage varchar(max)
+	,@ErrorMessage varchar(max)
 
--- Проверка на корректность загрузки
-	if not exists (
+                -- Проверка на корректность загрузки
+		if not exists (
 	select 1
-	from syn.ImportFile as f
-	where f.ID = @ID_Record
-		and f.FlagLoaded = cast(1 as bit)
+	from syn.ImportFile as imf
+	where imf.ID = @ID_Record
+		and imf.FlagLoaded = cast(1 as bit)
 	)
 		begin
-			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
+		set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 
 			raiserror(@ErrorMessage, 3, 1)
+	
 			return
 		end
 
-	CREATE TABLE #ProcessedRows(ActionType varchar(255), ID int)
+	create table #ProcessedRows (ActionType varchar(255), ID int)
 	
-	--Чтение из слоя временных данных
+	-- Чтение из слоя временных данных
 	select
 		cc.ID as ID_dbo_Customer
 		,cst.ID as ID_CustomerSystemType
@@ -32,7 +33,7 @@ begin
 		,cd.ID as ID_dbo_CustomerDistributor
 		,cast(isnull(sa.FlagActive, 0) as bit) as FlagActive
 	into #CustomerSeasonal
-	from syn.SA_CustomerSeasonal cs
+	from syn.SA_CustomerSeasonal as cs
 		join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
 			and cc.ID_mapping_DataSource = 1
 		join dbo.Season as s on s.Name = sa.Season
@@ -43,8 +44,10 @@ begin
 		and try_cast(sa.DateEnd as date) is not null
 		and try_cast(isnull(sa.FlagActive, 0) as bit) is not null
 
-	-- Определяем некорректные записи
-	-- Добавляем причину, по которой запись считается некорректной
+	/*
+	    Определяем некорректные записи.
+	    Добавляем причину, по которой запись считается некорректной
+	*/
 	select
 		sa.*
 		,case
@@ -60,7 +63,8 @@ begin
 	from syn.SA_CustomerSeasonal as cs
 	left join dbo.Customer as cc on cc.UID_DS = sa.UID_DS_Customer
 		and cc.ID_mapping_DataSource = 1
-	left join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor and cd.ID_mapping_DataSource = 1
+	left join dbo.Customer as cd on cd.UID_DS = sa.UID_DS_CustomerDistributor
+	        and cd.ID_mapping_DataSource = 1
 	left join dbo.Season as s on s.Name = sa.Season
 	left join syn.CustomerSystemType as cst on cst.Name = sa.CustomerSystemType
 	where cc.ID is null
